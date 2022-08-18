4a3cc42135c65b36d63d3a8602b2c1087aab2977
jayoo <jayooking@gmail.com>
2022-08-18 15:04:34 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="<jayooking>",
+    version="1.0.0",
+    author="<jayoo>",
+    author_email="<jayooking@gmail.com>",
+    description="<Todo Setup.py package>",
+    long_description="Another To-do list",
+    long_description_content_type="text/markdown",
+    url="<https://github.com/jayooking/yet_another_todo.git>",
+    packages=setuptools.find_packages(),
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10.3',
+)
